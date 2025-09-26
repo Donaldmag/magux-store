e206177ef77b15c00fa3aa810cba3c6f58c662c9
Donald Magoua <donald.m@glotelho.com>
2025-09-26 21:12:05 +0200

    Update:links and rounte

--- src/components/headerjsx/Navbar.jsx
+++ src/components/headerjsx/Navbar.jsx
@@ -1,17 +1,18 @@
+import { Link } from "react-router-dom";
 import maguxStoreLogo from "../../assets/magux-store.svg";
 import ComboBox from "./ComboBox";
 import SearchInput from "./SearchInput";
-import { Heart, ShoppingBag } from "lucide-react";
+import { Heart, ShoppingBag, Menu } from "lucide-react";
 
 function Navbar() {
   return (
     <>
       <header className="bg-transparent">
         <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-          <a className="flex items-center gap-3 text-blue-400" href="#">
+          <Link to="/" className="flex items-center gap-3 text-blue-400" href="#">
             <img src={maguxStoreLogo} alt="magux-store logo" />
             <h3 className="font-bold text-xl -mt-1">Magux Store</h3>
-          </a>
+          </Link>
 
           <div className="flex flex-1 items-center justify-end md:justify-between">
             <div className="hidden md:flex items-center gap-4">
@@ -20,27 +21,21 @@ function Navbar() {
             </div>
 
             <div className="flex items-center gap-4">
-              <div className="sm:flex sm:gap-4">
-                <Heart fill="black" strokeWidth={0} />
-                <ShoppingBag />
+              <div className="flex sm:gap-4 gap-4">
+                <Heart
+                  size={24}
+                  fill="black"
+                  className="icon-hover-animation"
+                />
+                <ShoppingBag
+                  color="#51a2ff"
+                  size={24}
+                  className="icon-hover-animation"
+                />
               </div>
 
-              <button className="block rounded-full bg-white shadow shadow-slate-200 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
-                <span className="sr-only">Toggle menu</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
+              <button className="block rounded-full bg-white shadow shadow-slate-200 p-1.5 text-gray-600 hover:text-gray-600/75 md:hidden icon-hover-animation">
+                <Menu size={16} />
               </button>
             </div>
           </div>
